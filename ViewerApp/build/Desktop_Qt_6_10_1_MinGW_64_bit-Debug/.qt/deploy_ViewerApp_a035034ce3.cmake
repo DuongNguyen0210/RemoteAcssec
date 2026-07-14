950b1a2323e04f75287b0769e500e4339b7626a6
DuongNguyen0210 <zkoclompe02@gmail.com>
2026-07-14 14:42:46 +0700

    Re System

--- ViewerApp/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/deploy_ViewerApp_a035034ce3.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/CloneCpp/ViewerApp/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/ViewerApp-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "D:/CloneCpp/ViewerApp/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/ViewerApp.exe"
-    GENERATE_QT_CONF
-)
