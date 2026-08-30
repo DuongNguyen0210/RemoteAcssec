2cecf5c9c5601f633cfcf626b0322b5f26486bae
JeiKei1206 <khoa.dinh1212@gmail.com>
2026-08-31 00:57:19 +0700

    Hoàn thiện lớp truyền tải RDTP an toàn
    - Bổ sung kiểm tra magic, version và payload length cho Relay ProtocolDecoder
    - Ngăn payload âm, quá giới hạn và integer overflow
    - Thêm ProtocolEncoder phía Java Relay
    - Thêm RdtpStreamParser phía C++ hỗ trợ TCP fragmentation/coalescing
    - Tái sử dụng ProtocolHeader và ProtocolSerializer hiện có
    - C++ build PASS, Java build PASS, Bionic review PASS

---
+++ clients/RemoteAccessApp/src/Network/protocol/RdtpStreamParser.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <QByteArray>
+#include <QList>
+
+#include "protocolheader.h"
+
+namespace Protocol {
+
+class RdtpStreamParser
+{
+public:
+    struct Message {
+        ProtocolHeader header;
+        QByteArray payload;
+    };
+
+    enum class Error {
+        None,
+        MalformedHeader,
+        InvalidMagic,
+        UnsupportedVersion,
+        PayloadTooLarge
+    };
+
+    struct FeedResult {
+        QList<Message> messages;
+        Error error = Error::None;
+    };
+
+    FeedResult feed(const QByteArray &bytes);
+
+private:
+    QByteArray m_buffer;
+};
+
+} // namespace Protocol
